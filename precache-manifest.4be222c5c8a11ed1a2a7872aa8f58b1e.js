ef828926ff8f4b6286f61ac3f152e3db756f9b77
Stefan Dunn <stefannicholasdunn@googlemail.com>
2020-10-18 15:58:07 +0100

    Updates

---
+++ precache-manifest.4be222c5c8a11ed1a2a7872aa8f58b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7414f45ef03ba4b0743eb0e46183e925",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6fa3e719e56192243eba",
+    "url": "./static/js/2.e52cd865.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e52cd865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f201c37797af3287fd5",
+    "url": "./static/js/main.d4fcf6b7.chunk.js"
+  },
+  {
+    "revision": "2065add6514001e16ce5",
+    "url": "./static/js/runtime-main.e5d417dd.js"
+  }
+]);
